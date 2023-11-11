2c579c975b711774e8c237718e39d03568b834da
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-11 18:46:20 +0100

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    EMBED_URL: "https://www.youtube.com/embed/",
-    STREAMING_SERVICE_URL: "https://byron-bay-tasmanian-devil-xdhf.2.ie-1.fl0.io",
-    YOUTUBE_TOKEN: "AIzaSyD0WVMzlUzHV_7pRvCbjYCM0I6qttwGVYc"
-};
